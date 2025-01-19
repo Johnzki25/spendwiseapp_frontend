2d4fe1e6ae17ec13674135ac3e827a4e87f80637
John Denrick <johnd.mf92@gmail.com>
2025-01-19 20:34:16 +0100

    Modified Header.tsx added LogoSection SearchBar and Button

--- app/Components/MainPage/Header.tsx
+++ app/Components/MainPage/Header.tsx
@@ -5,7 +5,7 @@ import AddOutlinedIcon from "@mui/icons-material/AddOutlined"; 66.7k (gzipped:24
 export default function Header(){
     return (
         <nav className="flex justify-between items-center">
-            {/* Logo Section*/}
+            {/* Logo Section */}
             <LogoSection />
             {/* SearchBar */}
             <SearchBar />
@@ -16,11 +16,41 @@ export default function Header(){
 }
 
 > function LogoSection() {
+      return (
+        <div className="flex gap-2 items-center">
+             {/* Icon Container */}
+        <div className={`bg-purple-600 flex items-center justify-center p-[11px] rounded-lg `}>
+             { /* Icon */}
+        <div className="w-[26px] h-[26px] items-center justify-center flex">
+            <SavingOutlinedIcon
+                sx={{ fontSize: "25px" }}
+                className="text-white" 
+           />
+         </div> 
+        </div>
 
+        {/* App Name */}
+        <div className="flex gap-1 text-[24px]">
+            <span className={`font-bold text-purple-600`}>Spend</span>
+            <span className="text-slate-600">Wise</span>
+          </div>  
+        </div>
+      );  
 }
 
 > function SearchBar(){
-
+    return (
+        <div className={`h-[46px] bg-slate-50 flex items-center text-sm rounded-md pl-3 gap-1 w-[300px] `}
+        >
+        <div>
+            <SearchOutlinedIcon className="text-slate-400" />        
+        </div>
+        <input
+            placeholder="Search an expense..."
+            className="bg-transparent outline-none w-full font-light" 
+          />    
+        </div>
+    );
 }
 
 function Button(){
@@ -29,5 +59,5 @@ function Button(){
             <AddOutlinedIcon sx=={{ fontSize: "20px"}} />
             <span className="">New Expense</span>
         </button>
-    )
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import Header from "./Components/Mainpage/Header";
+
 export default function Home() {
-  return <div className="poppins">Spend Wise</div>
+  return <div className="poppins w-full h-scren flex justify-center items-center bg-purple-600">
+    {/* Main Page Container */}
+    <div className="w-[59%] relative max-lg:w-[93%] h-[760px] bg-white shadow-lg rounded-2x1 p-12 px-[52px]">
+      {/* The App Header */}
+      <Header />
+    </div>
+    Spend Wise</div>
 }
